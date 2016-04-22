9210c16c4075faba1a94ac6c7151c9c8f0f7e127
ugorcak <ugorcak@gmx.ch>
2016-04-22 21:46:37 +0200

    patient added

--- src/model/Patient.java
+++ src/model/Patient.java
@@ -3,18 +3,38 @@ package model;
 /**
  * Created by Kajo on 4/22/2016.
  */
-public class Patient {
+public class Patient extends Person {
 
-    String name;
-    String firstName;
-    String street;
-    String city;
-    int birthyear;
+    // instance variables
+    private int patientNumber;
+    private String securityValue;
 
-    public Patient(String name, String firstName) {
-        this.name = name;
-        this.firstName = firstName;
+    // inherited name, firstname
+    public Patient(String aName, String aFirstname){
+        super(aName, aFirstname);
     }
 
+    // inherited second constructor
+    public Patient(int aPatNumber, String aName, String aFirstName, String aStreet, String aCity, int aBirthYear, String aSecValue){
+        super(aName, aFirstName, aStreet, aCity, aBirthYear);
+        this.patientNumber = aPatNumber;
+        this.securityValue = aSecValue;
+    }
+
+    public int getPatientNumber() {
+        return patientNumber;
+    }
+
+    public void setPatientNumber(int aPatientNumber) {
+        this.patientNumber = aPatientNumber;
+    }
+
+    public String getSecurityValue() {
+        return securityValue;
+    }
+
+    public void setSecurityValue(String aSecurityValue) {
+        this.securityValue = aSecurityValue;
+    }
 
 }
---
+++ src/model/Person.java
@@ -0,0 +1,65 @@
+package model;
+
+/**
+ * Created by Kajo on 4/22/2016.
+ */
+public class Person {
+
+    // instance variables
+    private String name;
+    private String firstName;
+    private String street;
+    private String city;
+    private int birthyear;
+
+    // main constructor
+    public Person(String aName, String aFirstName) {
+        this.name = aName;
+        this.firstName = aFirstName;
+    }
+
+    // additional constructor
+    public Person(String aName, String aFirstName, String aStreet, String aCity, int aBirthYear){
+        this.name = aName;
+        this.firstName = aFirstName;
+        this.street = aStreet;
+        this.city = aCity;
+        this.birthyear = aBirthYear;
+    }
+
+    private String getName(){
+        return name;
+    }
+    private void setName(String aName){
+        this.name = aName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String aFirstName) {
+        this.firstName = aFirstName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+    public void setStreet(String aStreet) {
+        this.street = aStreet;
+    }
+
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String aCity) {
+        this.city = aCity;
+    }
+
+    public int getBirthyear() {
+        return birthyear;
+    }
+    public void setBirthyear(int aBirthYear) {
+        this.birthyear = aBirthYear;
+    }
+
+}

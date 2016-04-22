562de2e1921e0da97255567a761ccc3b097482dc
ugorcak <ugorcak@gmx.ch>
2016-04-22 21:18:27 +0200

    initial commit

---
+++ src/model/Patient.java
@@ -0,0 +1,20 @@
+package model;
+
+/**
+ * Created by Kajo on 4/22/2016.
+ */
+public class Patient {
+
+    String name;
+    String firstName;
+    String street;
+    String city;
+    int birthyear;
+
+    public Patient(String name, String firstName) {
+        this.name = name;
+        this.firstName = firstName;
+    }
+
+
+}
